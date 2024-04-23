f810e33406892b643d2a904b41e859fd13921ded
Vipshal WG <119661093+vipshal-123@users.noreply.github.com>
2024-04-23 10:58:23 +0530

    login removed

--- notes-app/src/app/app.routes.ts
+++ notes-app/src/app/app.routes.ts
@@ -2,13 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-  { path:"home", component: HomeComponent },
-  { path:"", component: LoginComponent},
-  { path:"register", component: RegisterComponent},
+  { path:"", component: HomeComponent },
   { path:"about", component: AboutComponent },
   { path: "contact", component: ContactComponent}
 ];
